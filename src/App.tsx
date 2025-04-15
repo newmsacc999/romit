f1af80fbec3792a2db159c5f35027cfd472494c9
newmsacc999 <newmsacc999@hotmail.com>
2025-04-15 14:45:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './home.tsx'; 
+import SeatSelection from './pages/SeatSelection';
+import BookingConfirmation from './pages/BookingConfirmation';
+import PaymentOptions from './pages/PaymentOptions';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/select-seats/:matchId" element={<SeatSelection />} />
+        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
+        <Route path="/payment-options" element={<PaymentOptions />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
